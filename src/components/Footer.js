a6038fc5127f21d2257d00439ab71add05640116
Brian Leung <brianleung1017@gmail.com>
2020-06-09 01:27:41 +0800

    update style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,6 +16,10 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(5),
     paddingBottom: theme.spacing(5),
   },
+  title: {
+    fontSize: 16,
+    fontWeight: 700,
+  },
   text: {
     fontSize: 12,
   },
@@ -27,8 +31,9 @@ export default function Footer(props) {
   return (
     <div position="static" className={classes.root}>
       <Container maxWidth="lg">
-        <Typography variant="h6">Vote4HK 投票指南</Typography>
-        <p className={classes.text}>本網站與任何2019年區議會選舉候選人或其助選成員無關，刊載資料目的非為促使或阻礙任何候選人在選舉中當選。</p>
+        <Typography variant="h6" className={classes.title}>Vote4.hk 投票指南</Typography>
+        <p className={classes.text}>敝網站與任何2020年立法會選舉候選人或其助選成員無關，刊載資料目的非為促使或阻礙任何候選人在選舉中當選。</p>
+        <p className={classes.text}>敝網站所刊載資訊全為公開資料，刊載前已盡力確保資料真確性。</p>
       </Container>
     </div>
   )
