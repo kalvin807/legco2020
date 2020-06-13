ab866af9b5e57c75f606f002368fc2cf42d29519
Nandi Wong <nandi0315@gmail.com>
2020-06-13 17:22:52 +0000

    Deploy vote4hk/legco2020 to github.com/vote4hk/legco2020.git:gh-pages

---
+++ b98bc7c3-c6f62308757bdbb59823.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"29yR":function(n,t,a){"use strict";a.d(t,"a",(function(){return c}));var e=a("Lnxd"),c=function(n){return Object(e.a)({tag:"svg",attr:{version:"1",viewBox:"0 0 48 48",enableBackground:"new 0 0 48 48"},child:[{tag:"path",attr:{fill:"#F44336",d:"M34,9c-4.2,0-7.9,2.1-10,5.4C21.9,11.1,18.2,9,14,9C7.4,9,2,14.4,2,21c0,11.9,22,24,22,24s22-12,22-24 C46,14.4,40.6,9,34,9z"}}]})(n)};c.displayName="FcLike"}}]);
+//# sourceMappingURL=b98bc7c3-c6f62308757bdbb59823.js.map
